a8bfc5582bb4ce84df994db897334ceef822181e
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-02 16:17:14 -0300

    Adiciona módulos Customers e Favorites com controladores e serviços

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CustomersModule } from './customers/customers.module';
 import { ProductsModule } from './products/products.module';
-import { FavoritesModule } from './favorites/favorites.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+import { FavoritesController } from './favorites.controller';
 
-@Module({})
+@Module({
+  controllers: [FavoritesController],
+  providers: [FavoritesService],
+})
 export class FavoritesModule {}
