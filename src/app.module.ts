dfae0146db1ed43c25fb14340c5aeab4796aef8f
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-02 15:55:14 -0300

    Remove AppController and AppService, refactor AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { ProductsModule } from './products/products.module';
 import { FavoritesModule } from './favorites/favorites.module';
@@ -8,8 +6,12 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, ProductsModule, FavoritesModule, AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CustomersModule,
+    ProductsModule,
+    FavoritesModule,
+    AuthModule,
+    UsersModule
+  ]
 })
 export class AppModule {}
