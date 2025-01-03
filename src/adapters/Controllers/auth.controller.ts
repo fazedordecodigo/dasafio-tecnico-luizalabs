6d1b00dfecdfcd42113a8e6f237ee4589900e7f6
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-02 23:10:18 -0300

    Remove o uso do AuthGuard no endpoint de perfil e adiciona o decorador Public ao endpoint de login

--- src/adapters/Controllers/auth.controller.ts
+++ src/adapters/Controllers/auth.controller.ts
@@ -5,23 +5,22 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  UseGuards,
   Request,
 } from '@nestjs/common';
 import { AuthService } from '../Services/auth.service';
-import { AuthGuard } from '../Guards/auth.guard';
+import { Public } from '../constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
