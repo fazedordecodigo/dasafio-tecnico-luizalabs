14535ccc63fbaa8add4d0401ec6809d555a6684b
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-04 15:58:41 -0300

    Refatora a entidade Customer para utilizar importações de módulos com aliases

--- src/ports/Domain/entities/customer.entity.ts
+++ src/ports/Domain/entities/customer.entity.ts
@@ -1,17 +1,16 @@
-import { Entity } from "./entity";
-import { Product } from "./product.entity";
+import { Entity, Product } from '@ports/Domain/entities';
 
 export class Customer extends Entity {
-    public favorities?: Product[];
+  public favorities?: Product[];
 
-    constructor(
-        public name: string,
-        public email: string,
-        id?: string
-    ) {
-        super(id);
-        this.name = name;
-        this.email = email;
-        this.favorities = [];
-    }
+  constructor(
+    public name: string,
+    public email: string,
+    id?: string,
+  ) {
+    super(id);
+    this.name = name;
+    this.email = email;
+    this.favorities = [];
+  }
 }
