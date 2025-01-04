21ff199980feb66c34fd223cdf7374ba9db4a372
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-03 23:07:44 -0300

    Adiciona constante USER_REPOSITORY para protocolo do repositório de usuários

--- src/adapters/constants.ts
+++ src/adapters/constants.ts
@@ -8,6 +8,7 @@ export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 export const USER_SERVICE = 'UsersServiceProtocol';
+export const USER_REPOSITORY = 'UsersRepositoryProtocol';
 export const CUSTOMER_SERVICE = 'CustomerServiceProtocol';
 export const PRODUCT_SERVICE = 'ProductServiceProtocol';
 export const CUSTOMER_REPOSITORY = 'CustomersRepositoryProtocol';
