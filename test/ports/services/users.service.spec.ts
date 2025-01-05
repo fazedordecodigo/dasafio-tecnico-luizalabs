61a2c88ca4051908793f0e9eefc304ec1272458f
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-05 12:00:50 -0300

    adiciona teste para o método getByEmail no UsersService utilizando faker

--- test/ports/services/users.service.spec.ts
+++ test/ports/services/users.service.spec.ts
@@ -1,7 +1,8 @@
-import { USER_REPOSITORY, USER_SERVICE } from '@adapters/constants';
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { USER_REPOSITORY } from '@adapters/constants';
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersRepositoryProtocol } from '@ports/application/protocols';
 import { UsersService } from '@ports/application/services';
+import { UsersRepositoryProtocol } from '@ports/application/protocols';
 
 describe('UsersService', () => {
   let sut: UsersService;
@@ -13,17 +14,28 @@ describe('UsersService', () => {
         UsersService,
         {
             provide: USER_REPOSITORY,
-            useClass: jest.fn(),
+            useValue: {
+                getByEmail: jest.fn()
+            }
         }
       ],
     }).compile();
 
     sut = module.get<UsersService>(UsersService);
-    repository = module.get<UsersRepositoryProtocol>(USER_REPOSITORY);
+    repository = await module.resolve<UsersRepositoryProtocol>(USER_REPOSITORY);
 
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  describe('getById', () => {
+    it('should call repository.getByEmail with correct value', async () => {
+      const email = faker.internet.email()
+      const spy = jest.spyOn(repository, 'getByEmail');
+      await sut.getByEmail({ email });
+      expect(spy).toHaveBeenCalledWith(email);
+    });
   });
 });
