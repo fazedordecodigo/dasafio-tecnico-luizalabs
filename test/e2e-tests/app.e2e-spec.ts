4fd62ab98dcb442e337250e9caed5ea78765b6a1
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-05 19:09:38 -0300

    refatora estrutura de testes e2e, atualiza configuração e adiciona setup para banco de dados PostgreSQL

---
+++ test/e2e-tests/app.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { UsersController } from '@adapters/controllers/users.controller';
+import { PrismaService } from '@adapters/services/prisma.service';
+import { prismaService } from './setup-tests.e2e';
+import { AppModule } from '../../src/app.module';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).overrideProvider(PrismaService)
+      .useValue(prismaService)
+      .compile();
+
+    controller = module.get<UsersController>(UsersController);
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('users/ (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({
+        "email": faker.internet.email(),
+        "password": faker.internet.password(),
+        "role": "USER"
+       })
+      .expect(201);
+  });
+});
---
+++ test/e2e-tests/setup-tests.e2e.ts
@@ -0,0 +1,67 @@
+import { PrismaService } from '@adapters/services/prisma.service';
+import { Client } from 'pg';
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
+import { execSync } from 'node:child_process';
+
+let postgresContainer: StartedPostgreSqlContainer;
+let postgresClient: Client;
+let prismaService: PrismaService;
+let urlConnection: string;
+
+beforeAll(async () => {
+  postgresContainer = await new PostgreSqlContainer().start();
+
+  postgresClient = new Client({
+    host: postgresContainer.getHost(),
+    port: postgresContainer.getPort(),
+    database: postgresContainer.getDatabase(),
+    user: postgresContainer.getUsername(),
+    password: postgresContainer.getPassword(),
+  });
+
+  await postgresClient.connect();
+  process.env.DATABASE_URL = postgresContainer.getConnectionUri();
+  urlConnection = postgresContainer.getConnectionUri();
+  prismaService = new PrismaService({
+    datasources: {
+      db: {
+        url: urlConnection,
+      },
+    },
+    log: ['query'],
+  });
+  console.log('connected to test db...');
+});
+
+afterAll(async () => {
+  await prismaService.$disconnect();
+  await postgresClient.end();
+  await postgresContainer.stop();
+  console.log('test db stopped...');
+});
+
+beforeEach(async () => {
+  // drop schema and create a new one
+  execSync(`npx prisma migrate reset --force`, {
+    env: {
+      ...process.env,
+      DATABASE_URL: urlConnection,
+    },
+  });
+  execSync(`npx prisma migrate deploy`, {
+    env: {
+      ...process.env,
+      DATABASE_URL: urlConnection,
+    },
+  });
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+jest.setTimeout(80000);
+export { postgresClient, prismaService };
