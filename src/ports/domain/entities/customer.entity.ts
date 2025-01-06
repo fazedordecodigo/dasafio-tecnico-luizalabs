d99daca0b5c1119d64e86071a31e624407ec3003
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-05 21:03:19 -0300

    adiciona métodos para gerenciar favoritos no cliente, incluindo adição, remoção e recuperação

--- src/ports/domain/entities/customer.entity.ts
+++ src/ports/domain/entities/customer.entity.ts
@@ -2,7 +2,7 @@ import { Product } from '@ports/domain/entities';
 import { Entity } from './entity';
 
 export class Customer extends Entity {
-  public favorities?: Product[];
+  public favorities?: Product[] = [];
 
   constructor(
     public name: string,
@@ -12,6 +12,26 @@ export class Customer extends Entity {
     super(id);
     this.name = name;
     this.email = email;
-    this.favorities = [];
+  }
+
+  public addFavorite(product: Product): void {
+    const exists = this.favorities.find((p) => p.id === product.id);
+    if (exists) {
+      return;
+    }
+    this.favorities.push(product);
+  }
+
+  public removeFavorite(product: Product): void {
+    this.favorities = this.favorities.filter((p) => p.id !== product.id);
+  }
+
+  public getFavorites(): Product[] {
+    return this.favorities;
+  }
+
+  public update(data: Customer): void {
+    this.name = data.name;
+    this.email = data.email;
   }
 }
