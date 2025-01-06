11823952a2eb8f49a60a1a22080addbf682fcd27
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-05 23:02:08 -0300

    adiciona script de seed para popular o banco de dados com produtos e usuário admin

---
+++ prisma/seed.ts
@@ -0,0 +1,216 @@
+import { v7 } from 'uuid';
+import * as bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker/locale/pt_BR';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.createMany({
+    data: [
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+      {
+        id: v7(),
+        title: faker.commerce.productName(),
+        brand: faker.company.name(),
+        price: Number(faker.commerce.price()),
+        image: faker.image.dataUri({ type: 'svg-base64' }),
+        reviewScore: Number(
+          faker.finance.amount({
+            min: 0,
+            max: 5,
+            dec: 2,
+            symbol: '',
+            autoFormat: true,
+          }),
+        ),
+        createdAt: faker.date.recent(),
+        isDeleted: false,
+      },
+    ],
+  });
+  const salt = await bcrypt.genSalt();
+  const password = faker.internet.password();
+  const user = await prisma.user.create({
+    data: {
+      id: v7(),
+      email: faker.internet.email(),
+      password: await bcrypt.hash(password, salt),
+      role: 'ADMIN',
+      createdAt: faker.date.recent(),
+      isDeleted: false,
+    },
+  });
+  console.log(user, `password para acesso: ${password}`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
