4565f51997a0856b5081f5a2101cbc7a2eefa77a
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-05 21:01:20 -0300

    refatora constantes de adaptadores e adiciona decorador público

--- src/adapters/constants.ts
+++ src/adapters/constants.ts
@@ -1,14 +1,10 @@
-import { SetMetadata } from "@nestjs/common";
-
 export const jwtConstants = {
   secret: process.env.JWT_SECRET,
 };
 
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
-
 export const USER_SERVICE = 'UsersServiceProtocol';
 export const USER_REPOSITORY = 'UsersRepositoryProtocol';
-export const CUSTOMER_SERVICE = 'CustomerServiceProtocol';
-export const PRODUCT_SERVICE = 'ProductServiceProtocol';
+export const PRODUCT_SERVICE = 'ProductsServiceProtocol';
+export const PRODUCT_REPOSITORY = 'ProductsRepositoryProtocol';
+export const CUSTOMER_SERVICE = 'CustomersServiceProtocol';
 export const CUSTOMER_REPOSITORY = 'CustomersRepositoryProtocol';
