424c3e43408954e5f2bf17a05a96015e050a224e
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-06 15:15:30 -0300

    remove FavoritesService e atualiza o índice de serviços

--- src/ports/application/services/index.ts
+++ src/ports/application/services/index.ts
@@ -1,4 +1,3 @@
 export { UsersService } from '@ports/application/services/users.service';
 export { CustomersService } from '@ports/application/services/customers.service';
-export { FavoritesService } from '@ports/application/services/favorites.service';
 export { ProductsService } from '@ports/application/services/products.service';
