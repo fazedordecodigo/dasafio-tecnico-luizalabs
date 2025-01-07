06888e357485e034ce8cc7bc6ce6497faf6a9331
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-07 20:30:26 -0300

    altera nome do campo review para reviews na tabela Product e ajusta a criação de produtos no seed para incluir avaliações

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,194 +2,134 @@ import { v7 } from 'uuid';
 import * as bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker/locale/pt_BR';
-import { env } from 'process';
 
 const prisma = new PrismaClient();
-
 async function main() {
-  await prisma.product.createMany({
-    data: [
-      {
-        id: process.env.ID_PRODUCT_TO_GET_TEST_E2E,
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
+  await prisma.product.create({
+    data: {
+      id: process.env.ID_PRODUCT_TO_GET_TEST_E2E,
+      title: faker.commerce.productName(),
+      brand: faker.company.name(),
+      price: Number(faker.commerce.price()),
+      image: faker.image.dataUri({ type: 'svg-base64' }),
+      reviews: {
+        create: [
+          {
+            id: v7(),
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph(),
+            customer: faker.person.firstName(),
+            score: Number(
+              faker.finance.amount({
+                min: 0,
+                max: 5,
+                dec: 2,
+                symbol: '',
+                autoFormat: true,
+              })
+            ),
+            createdAt: faker.date.recent(),
+            isDeleted: false
+          },
+          {
+            id: v7(),
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph(),
+            customer: faker.person.firstName(),
+            score: Number(
+              faker.finance.amount({
+                min: 0,
+                max: 5,
+                dec: 2,
+                symbol: '',
+                autoFormat: true,
+              })
+            ),
+            createdAt: faker.date.recent(),
+            isDeleted: false
+          },
+          {
+            id: v7(),
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph(),
+            customer: faker.person.firstName(),
+            score: Number(
+              faker.finance.amount({
+                min: 0,
+                max: 5,
+                dec: 2,
+                symbol: '',
+                autoFormat: true,
+              })
+            ),
+            createdAt: faker.date.recent(),
+            isDeleted: false
+          },
+        ]
       },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
+      createdAt: faker.date.recent(),
+      isDeleted: false,
+    },
+  });
+  await prisma.product.create({
+    data: {
+      id: v7(),
+      title: faker.commerce.productName(),
+      brand: faker.company.name(),
+      price: Number(faker.commerce.price()),
+      image: faker.image.dataUri({ type: 'svg-base64' }),
+      reviews: {
+        create: [
+          {
+            id: v7(),
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph(),
+            customer: faker.person.firstName(),
+            score: Number(
+              faker.finance.amount({
+                min: 0,
+                max: 5,
+                dec: 2,
+                symbol: '',
+                autoFormat: true,
+              })
+            ),
+            createdAt: faker.date.recent(),
+            isDeleted: false
+          },
+          {
+            id: v7(),
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph(),
+            customer: faker.person.firstName(),
+            score: Number(
+              faker.finance.amount({
+                min: 0,
+                max: 5,
+                dec: 2,
+                symbol: '',
+                autoFormat: true,
+              })
+            ),
+            createdAt: faker.date.recent(),
+            isDeleted: false
+          }
+        ]
       },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-      {
-        id: v7(),
-        title: faker.commerce.productName(),
-        brand: faker.company.name(),
-        price: Number(faker.commerce.price()),
-        image: faker.image.dataUri({ type: 'svg-base64' }),
-        reviewScore: Number(
-          faker.finance.amount({
-            min: 0,
-            max: 5,
-            dec: 2,
-            symbol: '',
-            autoFormat: true,
-          }),
-        ),
-        createdAt: faker.date.recent(),
-        isDeleted: false,
-      },
-    ],
+      createdAt: faker.date.recent(),
+      isDeleted: false,
+    },
+  });
+  await prisma.product.create({
+    data: {
+      id: v7(),
+      title: faker.commerce.productName(),
+      brand: faker.company.name(),
+      price: Number(faker.commerce.price()),
+      image: faker.image.dataUri({ type: 'svg-base64' }),
+      createdAt: faker.date.recent(),
+      isDeleted: false,
+    },
   });
   const salt = await bcrypt.genSalt();
   const password = faker.internet.password();
