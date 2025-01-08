802bb7074d0f2d7cc319eae96b05b6c83a5fb596
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-08 09:41:10 -0300

    adiciona lógica para filtrar clientes excluídos e inclui avaliações nas consultas de favoritos

--- src/adapters/repositories/customers.repository.ts
+++ src/adapters/repositories/customers.repository.ts
@@ -8,13 +8,33 @@ export class CustomersRepository implements CustomersRepositoryProtocol {
   constructor(private readonly prisma: PrismaService) {}
 
   async getById(id: string): Promise<CustomerDto | null> {
+    const isDeleted = false;
     return await this.prisma.customer.findUnique({
-      where: { id },
+      where: { id, isDeleted },
+      include: {
+        favorites: {
+          include: {
+            reviews: true,
+          }
+        }
+      },
     });
   }
 
   async getAll(skip?: number, take?: number): Promise<CustomerDto[]> {
-    return await this.prisma.customer.findMany({ skip, take });
+    const isDeleted = false;
+    return await this.prisma.customer.findMany({ 
+      skip,
+      take,
+      where: { isDeleted },
+      include: {
+        favorites: {
+          include: {
+            reviews: true,
+          }
+        }
+      },
+    });
   }
 
   async create(data: CustomerDto): Promise<CustomerDto> {
@@ -24,15 +44,22 @@ export class CustomersRepository implements CustomersRepositoryProtocol {
   }
 
   async update(data: CustomerDto): Promise<CustomerDto> {
+    const isDeleted = false;
     return await this.prisma.customer.update({
       data,
-      where: { id: data.id },
+      where: { id: data.id, isDeleted },
     });
   }
 
   async delete(id: string): Promise<CustomerDto> {
-    return await this.prisma.customer.delete({
-      where: { id },
+    const isDeleted = false;
+    return await this.prisma.customer.update({
+      data: {
+        isDeleted: true,
+        updatedAt: new Date(),
+        deletedAt: new Date(),
+      },
+      where: { id, isDeleted },
     });
   }
 }
