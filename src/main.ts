394d32180871d1f52ff7e1827161f820bd6e4f64
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-08 14:35:49 -0300

    adiciona suporte ao Swagger para documentação da API e atualiza dependências no package.json e package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'module-alias/register'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , {
@@ -13,6 +14,18 @@ async function bootstrap() {
       transform: true,
     }
   ));
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio Luizalabs')
+    .setDescription('Api para cadastro de clientes e lista de produtos favoritos')
+    .setVersion('1.0')
+    .addTag('customers, products, favorites')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory), {
+    jsonDocumentUrl: 'swagger/json'
+  };
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
