cd66808c032982b4417ec14644d4497b3d6f0e82
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-09 17:20:24 -0300

    adiciona documentação Swagger ao DTO de favoritos

--- src/domain/dtos/customers/favorite.dto.ts
+++ src/domain/dtos/customers/favorite.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsUUID } from "class-validator";
 
 export class FavoriteDto {
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty({
+    description: 'The id of the favorite',
+  })
   id: string;
 }
