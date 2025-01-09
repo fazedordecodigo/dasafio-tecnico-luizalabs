c30cbe99e8e6a9d6ad11396467d9415787c9856a
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2025-01-09 16:25:57 -0300

    altera o seeder para criar clientes e produtos com dados fictícios, ajustando a estrutura de favoritos e revisões

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,75 +5,86 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 
 const prisma = new PrismaClient();
 async function main() {
-  await prisma.product.create({
+  await prisma.customer.create({
     data: {
-      id: process.env.ID_PRODUCT_TO_GET_TEST_E2E,
-      title: faker.commerce.productName(),
-      brand: faker.company.name(),
-      price: Number(faker.commerce.price()),
-      image: faker.image.dataUri({ type: 'svg-base64' }),
-      reviews: {
-        create: [
-          {
-            id: v7(),
-            title: faker.lorem.words(),
-            content: faker.lorem.paragraph(),
-            customer: faker.person.firstName(),
-            score: Number(
-              faker.finance.amount({
-                min: 0,
-                max: 5,
-                dec: 2,
-                symbol: '',
-                autoFormat: true,
-              })
-            ),
-            createdAt: faker.date.recent(),
-            isDeleted: false
-          },
-          {
-            id: v7(),
-            title: faker.lorem.words(),
-            content: faker.lorem.paragraph(),
-            customer: faker.person.firstName(),
-            score: Number(
-              faker.finance.amount({
-                min: 0,
-                max: 5,
-                dec: 2,
-                symbol: '',
-                autoFormat: true,
-              })
-            ),
-            createdAt: faker.date.recent(),
-            isDeleted: false
-          },
-          {
-            id: v7(),
-            title: faker.lorem.words(),
-            content: faker.lorem.paragraph(),
-            customer: faker.person.firstName(),
-            score: Number(
-              faker.finance.amount({
-                min: 0,
-                max: 5,
-                dec: 2,
-                symbol: '',
-                autoFormat: true,
-              })
-            ),
-            createdAt: faker.date.recent(),
-            isDeleted: false
-          },
-        ]
-      },
+      id: process.env.ID_CUSTOMER_TO_GET_TEST_E2E,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
       createdAt: faker.date.recent(),
       isDeleted: false,
+      favorites: {
+        create: [
+          {
+            id: process.env.ID_PRODUCT_TO_GET_TEST_E2E,
+            title: faker.commerce.productName(),
+            brand: faker.company.name(),
+            price: Number(faker.commerce.price()),
+            image: faker.image.dataUri({ type: 'svg-base64' }),
+            reviews: {
+              create: [
+                {
+                  id: process.env.ID_REVIEW_TO_GET_TEST_E2E,
+                  title: faker.lorem.words(),
+                  content: faker.lorem.paragraph(),
+                  customer: faker.person.firstName(),
+                  score: Number(
+                    faker.finance.amount({
+                      min: 0,
+                      max: 5,
+                      dec: 2,
+                      symbol: '',
+                      autoFormat: true,
+                    }),
+                  ),
+                  createdAt: faker.date.recent(),
+                  isDeleted: false,
+                },
+                {
+                  id: v7(),
+                  title: faker.lorem.words(),
+                  content: faker.lorem.paragraph(),
+                  customer: faker.person.firstName(),
+                  score: Number(
+                    faker.finance.amount({
+                      min: 0,
+                      max: 5,
+                      dec: 2,
+                      symbol: '',
+                      autoFormat: true,
+                    }),
+                  ),
+                  createdAt: faker.date.recent(),
+                  isDeleted: false,
+                },
+                {
+                  id: v7(),
+                  title: faker.lorem.words(),
+                  content: faker.lorem.paragraph(),
+                  customer: faker.person.firstName(),
+                  score: Number(
+                    faker.finance.amount({
+                      min: 0,
+                      max: 5,
+                      dec: 2,
+                      symbol: '',
+                      autoFormat: true,
+                    }),
+                  ),
+                  createdAt: faker.date.recent(),
+                  isDeleted: false,
+                },
+              ],
+            },
+            createdAt: faker.date.recent(),
+            isDeleted: false,
+          },
+        ],
+      },
     },
   });
   await prisma.product.create({
     data: {
-      id: v7(),
+      id: process.env.ID_PRODUCT_TO_POST_TEST_E2E,
       title: faker.commerce.productName(),
       brand: faker.company.name(),
       price: Number(faker.commerce.price()),
@@ -92,10 +103,10 @@ async function main() {
                 dec: 2,
                 symbol: '',
                 autoFormat: true,
-              })
+              }),
             ),
             createdAt: faker.date.recent(),
-            isDeleted: false
+            isDeleted: false,
           },
           {
             id: v7(),
@@ -109,12 +120,12 @@ async function main() {
                 dec: 2,
                 symbol: '',
                 autoFormat: true,
-              })
+              }),
             ),
             createdAt: faker.date.recent(),
-            isDeleted: false
-          }
-        ]
+            isDeleted: false,
+          },
+        ],
       },
       createdAt: faker.date.recent(),
       isDeleted: false,
@@ -132,18 +143,17 @@ async function main() {
     },
   });
   const salt = await bcrypt.genSalt();
-  const password = faker.internet.password();
-  const user = await prisma.user.create({
+  const password = process.env.PASSWORD_FAKE;
+  await prisma.user.create({
     data: {
       id: v7(),
-      email: faker.internet.email(),
+      email: process.env.EMAIL_FAKE,
       password: await bcrypt.hash(password, salt),
       role: 'ADMIN',
       createdAt: faker.date.recent(),
       isDeleted: false,
     },
   });
-  console.log(user, `password para acesso: ${password}`);
 }
 
 main()
